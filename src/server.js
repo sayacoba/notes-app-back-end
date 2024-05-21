863561c16894c6b8fce709c349d897188c224191
sayacoba sayabisa <sayacobasayabisa@gmail.com>
2024-05-21 15:35:32 +0800

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -1,72 +1,21 @@
-const http = require('http');
- 
-const requestListener = (request, response) => {
-    response.setHeader('Content-Type', 'text/html');
-    //response.statusCode = 200;
-    
-    const { method, url } = request;
- 
-    if(url === '/') {
-        if(method === 'GET') {
-            response.statusCode = 200;
-            //response.end('<h1>Ini adalah homepage</h1>');
-            response.end(JSON.stringify({
-                message: 'Ini adalah homepage',
-            }));
+const Hapi = require('@hapi/hapi');
+const got = require('./routes');
 
-        } else {
-            response.statusCode = 400;
-            //response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
-            response.end(JSON.stringify({
-                message: `Halaman tidak dapat diakses dengan ${method} request`,
-            }));
-        }
-    } else if(url === '/about') {
-        if(method === 'GET') {
-            response.statusCode = 200;
-            //response.end('<h1>Halo! Ini adalah halaman about</h1>')
-            response.end(JSON.stringify({
-                message: 'Halo! Ini adalah halaman about',
-            }));
-
-        } else if(method === 'POST') {
-            let body = [];
-    
-            request.on('data', (chunk) => {
-                body.push(chunk);
-            });
- 
-            request.on('end', () => {
-                body = Buffer.concat(body).toString();
-                const { name } = JSON.parse(body);
-                response.statusCode = 200;
-                //response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
-                response.end(JSON.stringify({
-                    message: `Halo, ${name}! Ini adalah halaman about`,
-                }));
-
-            });
-        } else {
-            response.statusCode = 400;
-            //response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
-            response.end(JSON.stringify({
-                message: `Halaman tidak dapat diakses menggunakan ${method}, request`
-            }));
-        }
-    } else {
-        response.statusCode = 404;
-       // response.end('<h1>Halaman tidak ditemukan!</h1>');
-        response.end(JSON.stringify({
-            message: 'Halaman tidak ditemukan!',
-        }));       
-    }
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+	routes: { 
+		cors: {
+			origin: ['*'],
+		},
+	},	
+  });
+  
+  server.route(routes);
+  
+  await server.start();
+  console.log(`server berjalan pada ${server.info.uri}`);
 };
- 
-const server = http.createServer(requestListener);
- 
-const port = 5000;
-const host = 'localhost';
- 
-server.listen(port, host, () => {
-    console.log(`Server berjalan pada http://${host}:${port}`);
-});
+
+init();  
